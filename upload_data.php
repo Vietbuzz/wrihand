2ccf75aa6544609130ad2ecf36bda04acf7cb207
buzz <buzzhoang@gmail.com>
2017-10-26 16:51:30 +0000

    number1

--- upload_data.php
+++ upload_data.php
@@ -6,6 +6,8 @@ $img = str_replace(' ', '+', $img);
 $data = base64_decode($img);
 $file = $upload_dir . mktime() . ".png";
 $success = file_put_contents($file, $data);
+shell_exec("python /home/pi/reply/tienlui.py 2>&1");
+
 print $success ? $file : 'Unable to save the file.';
 
 ?>
